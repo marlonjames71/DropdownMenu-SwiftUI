0514caa4475a4300600ddeb1e22ae3dfd5e08c8a
Marlon Raskin <marlonr@hey.com>
2023-03-12 19:54:44 -0400

    Initial Commit – Adds SwiftUI project

---
+++ DropdownMenu-SwiftUI/DropdownMenu-SwiftUI/DropdownMenu_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropdownMenu_SwiftUIApp.swift
+//  DropdownMenu-SwiftUI
+//
+//  Created by Marlon Raskin on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DropdownMenu_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DropdownMenu-SwiftUI/DropdownMenu-SwiftUIUITests/DropdownMenu_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DropdownMenu_SwiftUIUITestsLaunchTests.swift
+//  DropdownMenu-SwiftUIUITests
+//
+//  Created by Marlon Raskin on 3/12/23.
+//
+
+import XCTest
+
+final class DropdownMenu_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
