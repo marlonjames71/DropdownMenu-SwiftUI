03dd3901f5aacc0becb64875b2df9c5a5dc35f46
Marlon Raskin <marlonr@hey.com>
2023-03-15 00:31:00 -0400

    Adds DropdownMenu – very basic
    - Contains default mock menu items
    - No data is passed in

---
+++ DropdownMenu-SwiftUI/DropdownMenu-SwiftUI/Dropdown Menu/DropdownMenu.swift
@@ -0,0 +1,41 @@
+//
+//  DropdownMenu.swift
+//  DropdownMenu-SwiftUI
+//
+//  Created by Marlon Raskin on 3/14/23.
+//
+
+import SwiftUI
+
+struct DropdownMenu: View {
+    @State private var expanded = false
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Text("Choose an option")
+                Spacer()
+                Image(systemName: "chevron.right")
+                    .rotationEffect(.degrees(expanded ? 90 : 0))
+            }
+            .contentShape(Rectangle())
+            .onTapGesture {
+                withAnimation { expanded.toggle() }
+            }
+            
+            if expanded {
+                ForEach(MenuItem.mockMenuItems) { item in
+                    Text(item.title)
+                }
+            }
+        }
+        .padding()
+        .border(.red)
+    }
+}
+
+struct DropdownMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        DropdownMenu()
+    }
+}
